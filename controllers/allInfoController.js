be85612e1a34158be799dba97e53d9c90e7f7b64
Nishant-l <nishant3151998@gmail.com>
2021-10-13 18:25:53 +0530

    {...} :Converting info to js Object;

--- controllers/allInfoController.js
+++ controllers/allInfoController.js
@@ -13,5 +13,43 @@ module.exports.fetchInfo = (req,res)=>{
         res.render('allInfo',{students:student});
     })
 }
+// const obj = {
+//     name:student.name,
+//     email:student.email,
+//     batch:student.batch,
+// }
+// const finalObject =
+
+module.exports.download = (req,res)=>{
+    Student.find({})
+    .populate({
+        path:'interviews',
+        populate:{
+            path:'compony'
+        }
+    })
+    .exec((err,student)=>{
+        const finalObject = []
+        for(std of student){
+            const obj = {
+                name:std.name,
+                email:std.email,
+                batch:std.batch,
+            };
+            let c = 'compony';
+            let count = 1;
+            // console.log(std)
+            for(int of std.interviews){
+                obj[c+count] = int.compony.name;
+                obj[c+count+'result'] = int.result
+                // console.log(int);
+                count++;
+            }
+            finalObject.push(obj);
+        }
+        console.log(finalObject);
+        res.redirect('back');
+    })
+}
 
 
--- routs/allInfo.js
+++ routs/allInfo.js
@@ -4,5 +4,5 @@ const allInfoController = require('../controllers/allInfoController');
 const passport = require('passport');
 
 router.get('/',passport.checkAuthentication,allInfoController.fetchInfo);
-
+router.get('/download',passport.checkAuthentication,allInfoController.download);
 module.exports = router;;
